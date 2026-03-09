66cd0131de89eae8727f0df44de13ce99c1b41f1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 20:21:28 -0300

    subindo modificacoes

--- src/main/java/br/com/fiap/study_apir/PingController.java
+++ src/main/java/br/com/fiap/study_apir/PingController.java
@@ -5,11 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping()
 public class PingController {
 
     @GetMapping("/ping")
     public String ping(){
-        return "pong";
+        return "pong"; //Resposta padrão
+    }
+
+    @GetMapping("rota1")
+    public String rota1(){
+        return "rota1"; //Resposta padrão
+    }
+    @GetMapping("rota2")
+    public String rota2(){
+        return "rota2"; //Resposta padrão
     }
 }
